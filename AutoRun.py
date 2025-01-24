4b128359e33c47498f7bec27c9c83c139408a70c
Jasmin Lin <jazzhands2112@gmail.com>
2025-01-24 14:31:20 -0500

    fixed code

--- AutoRun.py
+++ AutoRun.py
@@ -1,29 +1,35 @@
 import os
 import subprocess
 
-path = "C:/Users/jlin3/gaussian-splatting"
+
+username = input('What is your username? ')
+print("Got it. You're ", username,"!")
+
+path = "C:/Users/" + username + "/gaussian-splatting"
+navigate_path = 'cd ' + path
+navigate_SIBR = 'cd C:/Users/' + username + '/gaussian-splatting/viewers/bin'
 
 subject = input('What is the name of your subject? ')
 print('Okay! Your subject has been named', subject,'!')
 
 mini_path_in = 'input_data/' + str(subject)
-mini_path_out = 'C:/Users/jlin3/gaussian-splatting/output/room/' + str(subject)
+mini_path_out = 'C:/Users/'+ username + '/gaussian-splatting/output/room/' + str(subject)
 
-colmap_execution = 'python convert.py -s ' + mini_path_in + ' --colmap_executable "C:/Users/jlin3/colmap-x64-windows-cuda/COLMAP.bat"'
-print(colmap_execution)
+colmap_execution = 'python convert.py -s ' + mini_path_in + ' --colmap_executable "C:/Users/' + username + '/colmap-x64-windows-cuda/COLMAP.bat"'
+# print(colmap_execution)
 
 optimizer = 'python train.py -s ' + mini_path_in
-print(optimizer)
+# print(optimizer)
 
 output_path = 'SIBR_gaussianViewer_app.exe -m ' + mini_path_out
-print(output_path)
+# print(output_path)
 
 commands = [
     'conda activate gaussian_splatting',
-    'cd C:/Users/jlin3/gaussian-splatting',
+    navigate_path,
     colmap_execution,
     optimizer,
-    'cd C:/Users/jlin3/gaussian-splatting-Windows/viewers/bin',
+    navigate_SIBR,
     output_path
 ]
 
